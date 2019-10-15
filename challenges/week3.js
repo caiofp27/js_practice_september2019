5e8a2c18555db71fb847b078a90aa8980a3d2926
Caio <caioymc@live.com>
2019-10-15 13:33:43 +0100

    Changed for in loops to regular for

--- challenges/week3.js
+++ challenges/week3.js
@@ -1,8 +1,8 @@
 function getSquares(nums) {
   if (nums === undefined) throw new Error("nums is required");
   let newArr = [];
-  for(let key in nums){
-    let square = nums[key]*nums[key];
+  for(let i=0; i<nums.length; i++){
+    let square = nums[i]*nums[i];
     newArr.push(square);
   }
   return newArr;
@@ -25,9 +25,9 @@ function camelCaseWords(words) {
 function getTotalSubjects(people) {
   if (people === undefined) throw new Error("people is required");
   let count = 0;
-  for(let key in people){
-    if(people[key].subjects.length > 0){
-      count = count + people[key].subjects.length;
+  for(let i=0; i<people.length; i++){
+    if(people[i].subjects.length > 0){
+      count = count + people[i].subjects.length;
     }
   }
   return count;
@@ -36,10 +36,10 @@ function getTotalSubjects(people) {
 function checkIngredients(menu, ingredient) {
   if (menu === undefined) throw new Error("menu is required");
   if (!ingredient) throw new Error("ingredient is required");
-  for(let key in menu){
-    let items = menu[key].ingredients;
-    for(let i in items){
-      if(items[i] === ingredient){
+  for(let i=0; i<menu.length; i++){
+    let items = menu[i].ingredients;
+    for(let j=0; j<items.length; j++){
+      if(items[j] === ingredient){
         return true;
       }
     }
@@ -51,8 +51,8 @@ function duplicateNumbers(arr1, arr2) {
   if (arr1 === undefined) throw new Error("arr1 is required");
   if (arr2 === undefined) throw new Error("arr2 is required");
   let newArr = [];
-  for(let i in arr1){
-    for(let j in arr2){
+  for(let i=0; i<arr1.length; i++){
+    for(let j=0; j<arr2.length; j++){
       if(arr1[i] === arr2[j]){
         let n = arr1[i];
         if(!newArr.includes(n)){
